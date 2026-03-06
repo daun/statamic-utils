c3fb4d86cd9f1a1518cfedf9a53b0298a453afc5
Philipp Daun <post@philippdaun.net>
2026-03-06 21:59:15 +0100

    Improve relation title search transformer
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/Search/Transformers/RelationshipTitle.php
+++ src/Search/Transformers/RelationshipTitle.php
@@ -2,25 +2,43 @@
 
 namespace Daun\StatamicUtils\Search\Transformers;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Statamic\Contracts\Query\Builder;
+use Statamic\Entries\Entry;
+use Statamic\Taxonomies\LocalizedTerm;
+use Statamic\Taxonomies\Term;
 
 class RelationshipTitle
 {
     public function handle($value, $field = null, $searchable = null)
     {
-        if (! $value) {
-            return null;
-        }
+        $items = $this->augment($searchable, $field);
 
+        $value = match (true) {
+             $items instanceof Collection => $items->map($this->title(...))->filter()->values()->all(),
+             default => Arr::wrap($this->title($items)),
+        };
+
+        return [$field => $value];
+    }
+
+    public function augment($searchable, $field)
+    {
         $items = $searchable->augmentedValue($field)->value();
 
-        if ($items instanceof Collection) {
-            return $items->map->get('title')->all();
-        } elseif ($items instanceof Builder) {
-            return $items->get()->pluck('title');
-        } else {
-            return $items->title ?? null;
-        }
+        return $items instanceof Builder
+            ? $items->get()
+            : $items;
+    }
+
+    public function title($item)
+    {
+        return match (true) {
+             $item instanceof Entry => $item->value('title'),
+             $item instanceof Term => $item->value('title'),
+             $item instanceof LocalizedTerm => $item->value('title'),
+             default => $item->title ?? null,
+        };
     }
 }
