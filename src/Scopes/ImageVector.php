888b582d03462122a30351a7f1639e854bc1f9e6
Philipp Daun <post@philippdaun.net>
2026-06-17 09:06:03 +0200

    Code style updates

--- src/Scopes/ImageVector.php
+++ src/Scopes/ImageVector.php
@@ -2,6 +2,7 @@
 
 namespace Daun\StatamicUtils\Scopes;
 
+use Statamic\Query\Builder;
 use Statamic\Query\Scopes\Scope;
 
 class ImageVector extends Scope
@@ -9,7 +10,7 @@ class ImageVector extends Scope
     /**
      * Apply the scope.
      *
-     * @param  \Statamic\Query\Builder  $query
+     * @param  Builder  $query
      * @param  array  $values
      * @return void
      */
